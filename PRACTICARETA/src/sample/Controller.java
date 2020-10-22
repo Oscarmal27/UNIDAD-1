8f39ef3c27dd1e43413a4b44c846fcf0eed90993
Oscarmal27 <71733530+Oscarmal27@users.noreply.github.com>
2020-10-22 01:40:22 -0600

    Continuacion

--- PRACTICARETA/src/sample/Controller.java
+++ PRACTICARETA/src/sample/Controller.java
@@ -10,6 +10,7 @@ import java.io.FileReader;
 
 public class Controller {
     @FXML TextArea txtReceta, txtIngre, txtProce, txtPconr;
+    //String texto="";
 
     @FXML protected void initialize(){
         try {
@@ -26,14 +27,30 @@ public class Controller {
 
     public void procesar (ActionEvent event){
         String[] arregloLineas= txtReceta.getText().split("\n");
-        boolean encontro=false;
-        for (int x=0; x<arregloLineas.length;x++) {
-            if (encontro){
-                txtIngre.setText(arregloLineas[x]);
+        int ancho=0;
+        int guajillo=0;
+        int pasilla=0;
+        for (int x=0;x<arregloLineas.length;x++) {
+            if (x==4){
+                String[] arrAncho=arregloLineas[x].split(" ");
+                ancho= Integer.parseInt(arrAncho[0]);
             }
-            if (arregloLineas[x].contains("-------")){
-                encontro=true;
+            if (x==5){
+                String[] arrGuajillo=arregloLineas[x].split(" ");
+                guajillo= Integer.parseInt(arrGuajillo[0]);
             }
+            if (x==6){
+                String[] arrpasilla=arregloLineas[x].split(" ");
+                pasilla= Integer.parseInt(arrpasilla[0]);
+            }
+            txtIngre.setText(arregloLineas[3]+"\n"+arregloLineas[4]+"\n"+arregloLineas[5]+
+                    "\n"+arregloLineas[7]+"\n"+arregloLineas[8]+"\n"+arregloLineas[9]+"\n"+arregloLineas[10]+
+                    "\n"+arregloLineas[11]+"\n"+arregloLineas[12]+"\n"+arregloLineas[13]+"\n"+arregloLineas[14]+
+                    "\n"+arregloLineas[15]+"\n"+arregloLineas[16]+"\n"+arregloLineas[17]);
+            txtProce.setText(arregloLineas[20]+"\n"+arregloLineas[21]+"\n"+arregloLineas[23]+
+                    "\n"+arregloLineas[25]+"\n"+arregloLineas[27]+"\n"+arregloLineas[29]+"\n"+arregloLineas[31]);
+            txtPconr.setText(arregloLineas[34]+"\n"+ (ancho+guajillo+pasilla) +" gramos"+"\n"+" "+"\n"+arregloLineas[35]+"\n"+arregloLineas[25]+
+                    "\n"+" "+"\n"+arregloLineas[36]+"\n"+arregloLineas[7]+"\n"+arregloLineas[8]+"\n"+arregloLineas[14]+"\n"+" "+"\n"+arregloLineas[37]+"\n"+" ");
         }//llave for
     }//llave procesar
 }//llave Controller
